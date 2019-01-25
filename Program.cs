1f7b7b689c61b9bc0dcad6e1fe697721dd012264
Harika Naidu <nagaharika.n@gmail.com>
2019-01-24 21:13:52 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        public decimal totalCost(string itemNo, int quantity, decimal unitCost)
+        {
+            decimal totalCost = 0.0m;
+            switch (itemNo)
+            {
+                case "1":
+                {
+                    totalCost = unitCost * quantity;
+                    break;
+                }
+                case "2":
+                {
+                    totalCost = unitCost * quantity;
+                    break;
+                }
+                case "3":
+                {
+                    totalCost = unitCost * quantity;
+                    break;
+                }
+                case "4":
+                {
+                    totalCost = unitCost * quantity;
+                    break;
+                }
+                default:
+                {
+                    totalCost = totalCost + (10*quantity);
+                    break;
+                }
+            }    
+            return totalCost;
+        }
+        static void Main(string[] args)
+        {
+            bool exitOpti = true;
+            decimal unitCost = 0.0m;
+            var purchaseCost = 0.0m;
+            string customerName="";
+            int age;
+            int quantity =0;
+            String itemNo;
+            while(exitOpti)
+            {
+                customerName="";
+                age =0;
+                while(customerName=="")   
+                {
+                    Console.WriteLine("Enter your Name:");
+                    customerName=Console.ReadLine();
+                }
+                while(age<=0)
+                {
+                    Console.WriteLine("Enter your age");
+                    age=Convert.ToInt32(Console.ReadLine());
+                }
+                if(age>=21)
+                {
+                    Console.WriteLine("Alcohol Menu");
+                    Console.WriteLine("\n\t 1.Black and white blended scotch");
+                    Console.WriteLine("\n\t 2.Kingfisher Bagpiper");
+                    Console.WriteLine("\n\t 3.Knock Out Brandy");
+                    Console.WriteLine("\n\t 4.Imperier Blue");
+                    Console.WriteLine("Enter the Alcohol you want to buy from the list: ");
+                    itemNo = Console.ReadLine();
+                    if(itemNo == "1")
+                    {
+                        unitCost =12;
+                    }
+                    else if(itemNo == "2")
+                    {
+                        unitCost = 14;
+                    }
+                    else if(itemNo == "3")
+                    {
+                        unitCost = 20;
+                    }
+                    else if(itemNo == "4")
+                    {
+                        unitCost = 24;
+                    }
+                    Console.WriteLine("Enter the quantity you need to buy"); 
+                    quantity = Convert.ToInt32(Console.ReadLine()); 
+                    Program total = new Program();
+                    purchaseCost = total.totalCost(itemNo, quantity, unitCost);
+                    Console.WriteLine("=================PRICING=============");
+                    Console.WriteLine($"Alcohol {itemNo} Total Purchase Cost is: {purchaseCost}$");
+                    Console.WriteLine("=====================================");
+                }
+                else 
+                {
+                    Console.WriteLine("You are not eligible to buy.");   
+                }
+                Console.WriteLine("Enter true to continue again or q to quit :");        
+                var ch = Console.ReadLine();
+                if(ch == "q")
+                {
+                    Console.WriteLine("You are Done!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+Author: Harika Naidu
+44663 P1 assignment
+
+
+At the Starting of my assignment I have created a Repository in my github account and named it as the P1. I have clone that assignment and copied it into my local repository. 
+
+In the Power shell 
+1. run with the command dontnet new console.
+2. dotnet run.
+
+This app is containing the boole, int, double, char, string and decimal type of values.
+
+This app contains the while loop which haves the condition.
+
+My app also contains the selection statments(Switch) and the branching statments like if and if-else.
+
+Here in my app I have implemented the a static function called public decimal totalCost(string itemNo, int quantity, decimal unitCost). Which is used to calculate the total bill of the customer on which Alcohol the customer has sent.
+
+This Method takes the itemNno, qunatity and unnitCost as a parameters and find the total purchase amount that the customer has buy on the Alcohol.
+
+Finally my app will calculated the totalCost of the customer purchased.
+
+Here in the app it will ask you to enter true to continue or q to quit.
+
+
+
+My Repository Link:
+
+
+
